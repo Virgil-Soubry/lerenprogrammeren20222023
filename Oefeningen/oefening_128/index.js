e542753a7606705c6df5a5fec4bc1d226646e73a
Virgil Soubry <virgil.soubry@gmail.com>
2022-09-15 11:02:13 +0200

    oefening_128

--- Oefeningen/oefening_128/index.js
+++ Oefeningen/oefening_128/index.js
@@ -4,12 +4,12 @@ let y = 1;
 let opvangbak = ""
 
 for (i; i <= ingegevenGetal; i++) {
+
     if (y <= ingegevenGetal) {
         opvangbak = opvangbak + y + ",";
-    } else if (y <= ingegevenGetal) {
-
-        console.log(y);
+        // console.log(y);
     }
     y *= 2;
 }
+console.log(opvangbak.substring(0,opvangbak.length-1));
 
