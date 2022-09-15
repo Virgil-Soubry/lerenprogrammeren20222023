159fd186edcef720dd94e2c1d4438c88cef8252a
Virgil Soubry <virgil.soubry@gmail.com>
2022-09-15 11:55:23 +0200

    oefening_129

--- Oefeningen/oefening_129/index.js
+++ Oefeningen/oefening_129/index.js
@@ -1,14 +1,17 @@
-let gevraagd = Number(prompt("Voer een getal in."));
-let i = 1;
+let gevraagd = Number(prompt("Voer een getal in:"));
 
-for (i; i <= gevraagd; i++) {
 
-    for (i; i <= gevraagd; i++) {
-        i % i === 0;
-        i % 1 === 0;
-
+for (let i = 1; i < gevraagd; i++) {
+    var count = 0;
+    for (let x = 1; x <= i; x++) {
+        if (i % x === 0) {
+            count++
+        }
     }
+    if (count === 2) {
+        console.log(i)
+    }
+}
 
 
-}console.log(i)
-
+//y % y === 0;
