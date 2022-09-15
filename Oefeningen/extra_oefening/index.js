581bceb20f5fa7defd38020510e28228d6505400
Virgil Soubry <virgil.soubry@gmail.com>
2022-09-15 13:42:53 +0200

    extar_oefening

---
+++ Oefeningen/extra_oefening/index.js
@@ -0,0 +1,29 @@
+/*
+Vraag raad het nummer?
+-groter of kleiner
+-aantal dat keer geraden is
+-resultaat in hoeveel pogingen
+ */
+
+
+let nummer = 67;
+var ingave = Number(prompt("Raad het getal:"));
+var count = 0;
+for (let i = 1; i > 0; i++) {
+
+    if (ingave > nummer) {
+        alert("KLEINER, probeer opnieuw");
+        count++;
+        ingave = Number(prompt("Raad het getal:"));
+    } else if (ingave < nummer) {
+        alert("GROTER, probeer opnieuw");
+        count++;
+        ingave = Number(prompt("Raad het getal:"));
+    } else {
+        count++;
+        console.log("goed geraden het getal is", nummer, "je hebt er", count, "keer over geraden");
+        alert("goed geraden");
+        i = -1;
+    }
+
+}
--- Oefeningen/oefening_1210/index.js
+++ Oefeningen/oefening_1210/index.js
@@ -1,2 +1,3 @@
-
+let tafel = Number(prompt("geef een tafel(getal) in"));
+let eindGetal = Number(prompt("Geef een eind getal in:"))
 
